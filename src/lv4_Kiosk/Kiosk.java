0a6197aeb7ed7ff6fe8ab396559ab4b264548450
dandylsj <lsjyahoo@gmail.com>
2025-10-28 17:00:19 +0900

    feat: Lv4 키오스크  Kiosk 클래스 리펙토링
    1.어떤것을 지칭하는지 헷갈리는 변수명들을 조금 더 명확하게 어떤것들을 지칭하는지 변경
    1-1.Menu 클래스 List의 변수명을 topMenu에서 category로 변경
    2.MenuItem 리스트를 Menu 클래스의 메서드에서 각 기능별 메뉴를 불러오던 방식에서,
    카테고리 입력 숫자에 맞는 하위 메뉴아이템 리스트를 불러올 수 있게 만듬.
    3.코드의 가시성이 잘 보이도록 단계별로 나누어 주석을 추가하였음.

--- src/lv4_Kiosk/Kiosk.java
+++ src/lv4_Kiosk/Kiosk.java
@@ -8,46 +8,54 @@ import java.util.Scanner;
 public class Kiosk {
 
     //1.속성 (Field)
-    List <Menu> topMenu; //카테고리 메뉴(상위 메뉴)의 배열을 불러옴
+    private Menu selectedCategory; //메뉴카테고리 선택값을 위한 속성
+    private List<Menu> category; //카테고리 메뉴(상위 메뉴)의 배열을 불러옴
     Scanner sc = new Scanner(System.in); // Scanner 선언
 
 
     //2.생성자 (Constructor)
-    Kiosk(List<Menu> topMenu) {  //List<MenuItem> menuItems 는 Kiosk 클래스 생성자를 통해 값을 할당합니다.
-
-        this.topMenu = topMenu;
+    Kiosk(List<Menu> category) {  //List<MenuItem> menuItems 는 Kiosk 클래스 생성자를 통해 값을 할당합니다.
+        this.category = category;
     }
-
 
     //3. 기능 (Method)
     public void start() { //main 함수에서 관리하던 입력과 반복문 로직은 이제 start 함수를 만들어 관리합니다.
+      while (true) {  //키오스크 내부의 기능을 반복하기위한 반복문
+          //1.메뉴 리스트 출력
+          System.out.println("[ MAIN MENU ]"); //카테고리 메뉴(상위 메뉴) 출력  for문을 사용하여 List의 메뉴를 출력합니다.
+          for (int i = 0; i < category.size(); i++) {
+              System.out.println(i + 1 + "." + category.get(i).getCategory());
+          }
+          System.out.println("0 을 누르면 키오스크를 종료합니다.");
 
-        Menu menu1 = topMenu.get(0);//카테고리 메뉴의 0번째 배열 불러오기
+          //2. 사용자 입력값 받기
+          int selectCategoryNumber = sc.nextInt(); //스캐너에 넣은 정수값을 selectTopMenuNumber 에 넣는다.
 
-        //MenuItem 클래스 인스턴스
+          if (selectCategoryNumber == 0) {  //0번 값을 입력하면  키오스크를 종료하도록 한다.
+              System.out.println("키오스크를 종료합니다.");
+              System.exit(0); //시스템 종료 선언
+          } else {
+              //3. 사용자 선택한 메뉴
+              selectedCategory = category.get(selectCategoryNumber - 1);//카테고리 메뉴에서 선택한 값의 -1 배열에 있는 카테고리를 selectedCategory에 넣는다.
+          }
 
-        // 반복문을 활용해 List 안에 있는 MenuItem을 하나씩 출력
-        while (true) {
-            System.out.println("[ MAIN MENU ]"); //카테고리 메뉴(상위 메뉴) 출력  for문을 사용하여 List의 메뉴를 출력합니다.
-            for (int i = 0; i < topMenu.size(); i++) {
-                System.out.println(i + 1 + "." + topMenu.get(i).getTopMenu());
-            }
-            System.out.println("0 을 누르면 키오스크를 종료합니다.");
-            int selectTopMenuNumber = sc.nextInt(); //스캐너에 넣은 정수값을 selectTopMenuNumber 에 넣는다.
-            if (selectTopMenuNumber == 0) {  //0번 값을 입력하면  키오스크를 종료하도록 한다.
-                System.out.println("키오스크를 종료합니다.");
-                System.exit(0); //시스템 종료 선언
-            } else if(selectTopMenuNumber == 1){ //1을 누를경우 1번의 버거 하위메뉴들이 출력된다.
-                System.out.println("[" +  topMenu.get(0).getTopMenu() + "MENU" + "]");
-                menu1.burgerMenuItems();
-            } else if(selectTopMenuNumber == 2) { //2를 누를경우 2번의 음료 하위메뉴들이 출력된다.
-                System.out.println("[" +  topMenu.get(1).getTopMenu() + "MENU" + "]");
-                menu1.drinkMenuItems();
-            } else if(selectTopMenuNumber == 3) { //3을 누를경우 3번의 디저트 하위메뉴들이 출력된다.
-                System.out.println("[" +  topMenu.get(2).getTopMenu() + "MENU" + "]");
-                menu1.dessertMenuItems();
-            }
 
-        }
+          while (true) {  //하위 메뉴 출력을 반복하기위한 반복문
+              //4. 하위메뉴 출력
+              selectedCategory.printMenuItems();//2번에서 선택한 하위 메뉴가 출력된다.
+              //5. 사용자 입력값받기
+              int selectMenuItemNumber = sc.nextInt();
+              if (selectMenuItemNumber == 0) { //0번 값을 입력하면 카테고리 로 넘어간다.
+                  System.out.println("카테고리 메뉴로 넘어갑니다.");
+                  break; //카테고리로 넘어갑니다.
+              }
+              //6.선택한 메뉴가 출력
+              List<MenuItem> menuItems = selectedCategory.getMenuItems(); //선택한 카테고리의 메뉴아이템을 불러오는 기능
+              MenuItem selectedMenuItem = menuItems.get(selectMenuItemNumber - 1); //5 에서 선택한 숫자를 -1 한 배열의 값을 불러오는 기능
+
+              System.out.println("선택한 메뉴 아이템 : " + selectedMenuItem); //5번에서 선택한 정수의 배열을 문자로 불러온다.
+          }
+      }
     }
 }
+
