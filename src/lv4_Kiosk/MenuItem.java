97711255f1f0449904168f6f901673d09faec65b
dandylsj <lsjyahoo@gmail.com>
2025-10-28 17:10:57 +0900

    feat: Lv4 키오스크  MenuItem 클래스 리펙토링
    1. Kiosk 클래스에서 출력하는 하위메뉴를 선택할때 주소값이 뜨지 않도록 toString 을 사용하여 메뉴이름 가격 상세정보고 뜰 수 있는 메서드 추가

--- src/lv4_Kiosk/MenuItem.java
+++ src/lv4_Kiosk/MenuItem.java
@@ -1,34 +1,31 @@
 package lv4_Kiosk;
 
 public class MenuItem {
-    //1.속성
+    //1.속성 Filed
     private String name;  //메뉴 이름 문자 속성
     private double price; //메뉴 가격 double 속성
     private String content; //메뉴 내용 문자 속성
 
-    //2.생성자
+    //2.생성자 Constructor
     MenuItem(String name,double price,String content){
         this.name = name;
         this.price = price;
         this.content = content;
     }
-
-    //3.기능
+    //3.기능 Method
     String getName(){
-
         return name;
     }
     double getPrice(){
-
         return price;
     }
     String getContent(){
-
         return content;
     }
-
-
-
+    @Override  // Kiosk 클래스 에서 하위메뉴를 문자로 출력하기위한 기능
+    public String toString() {
+        return name + "| W"+ price + " | " + content ;
+    }
 }
 
 
